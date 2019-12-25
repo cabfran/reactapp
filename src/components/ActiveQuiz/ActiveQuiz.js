391cd411f9de4a1e374a969bf0927ae599d97162
Anat <>
2019-12-25 22:38:49 +0300

    add css styles

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import classes from './ActiveQuiz.module.css'
+import React from "react";
+import classes from "./ActiveQuiz.module.css";
 
 const ActiveQuiz = props => (
-<div className={classes.ActiveQuiz}>
-<p>
-  <span>
-    <strong>2.
-      Как дела? </strong>
-  </span>
-</p>
+  <div className={classes.ActiveQuiz}>
+    <p className={classes.Questions}>
+      <span>
+        <strong>2.&nbsp; Как дела? </strong>
+      </span>
 
+      <small>4 of 12</small>
+    </p>
 
-<ul>
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-  <li>4</li>
-  <li>5</li>
-</ul>
-</div>
-)
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+  </div>
+);
 
-export default ActiveQuiz
+export default ActiveQuiz;
